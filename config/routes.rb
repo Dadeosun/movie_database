a220a7e7e19fc75a2c203c5c037bff7697d548e9
Raghu Betina <raghu@firstdraft.com>
2017-04-27 23:52:12 +0000

    Generate movies resource with devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :movies
+  # Routes for the Movie resource:
+  # READ
+  get "/movies", :controller => "movies", :action => "index"
+  get "/movies/:id", :controller => "movies", :action => "show"
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
